186c0811ec8de6441170972fd945c1ace1a706b8
Mubasher <mubasheriqbal693@gmail.com>
2023-02-01 21:28:24 +0500

    remace ' with tillda

--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -4,14 +4,13 @@ const github = require('@actions/github');
 try {
     //Get input
     const name = core.getInput("who-to-greet");
-    console.log(`Hello ${name}`)
-
+    console.log(`Hello ${name}`);
     // post output
     const time = new Date();
     core.setOutput('time', time.toTimeString());
 
     // Check the context of out action.
-    console.log(JSON.stringify(github, null, '/t'));
+    console.log(JSON.stringify(github, null, "\t"));
 } catch (e){
     core.setFailed(e.message);
 }
